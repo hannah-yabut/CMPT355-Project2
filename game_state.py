3b362c03461c59bce15ff6a153b069d863ddbf51
MathPRG <gabriel.w.young@outlook.com>
2026-03-24 04:58:16 -0600

    Encapsulated logic and modified to use bitboards

--- game_state.py
+++ game_state.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from board import Board, board_apply_move, board_legal_moves, board_opponent
+from board import board_apply_move, board_legal_moves, board_opponent
 from move import Move
 
 
@@ -12,25 +12,23 @@ class GameState:
     - applying moves 
     - switching turns 
     '''
-    pass
+    
+    def __init__(self, blackBoard: int, whiteBoard: int, player_to_move: str):
+        """Creates a GameState from the initial data given."""
 
+        self.black_board = blackBoard
+        self.white_board = whiteBoard
+        self.player_to_move = player_to_move
 
-def init_game_state(state: GameState, board: Board, player_to_move: str) -> None:
-    state.board = board
-    state.player_to_move = player_to_move
+    def get_legal_moves(self) -> List[Move]:
+        """Returns a list of all valid moves from this state."""
 
+        return board_legal_moves(self.black_board, self.white_board, self.player_to_move)
+    
+    def get_result_state(self, move: Move) -> "GameState":
+        """Returns a new GameState which is the result of making the given move from this state."""
+        
+        new_black, new_white = board_apply_move(self.black_board, self.white_board, move)
+        next_player = board_opponent(self.player_to_move)
 
-def create_game_state(board: Board, player_to_move: str) -> GameState:
-    state = GameState()
-    init_game_state(state, board, player_to_move)
-    return state
-
-
-def game_state_legal_moves(state: GameState) -> List[Move]:
-    return board_legal_moves(state.board, state.player_to_move)
-
-
-def game_state_next_state(state: GameState, move: Move) -> GameState:
-    new_board = board_apply_move(state.board, move, state.player_to_move)
-    next_player = board_opponent(state.player_to_move)
-    return create_game_state(new_board, next_player)
+        return GameState(new_black, new_white, next_player)
