c3a35badd0b9243a7424d4a02d23b00b62421754
MathPRG <gabriel.w.young@outlook.com>
2026-04-06 12:08:08 -0600

    Removed outdated comments left behind from the 32-bit implementation of the bitboards

--- board.py
+++ board.py
@@ -6,7 +6,8 @@ from utils import BOARD_SIZE
 BOARD_EMPTY = "O"
 BOARD_BLACK = "B"
 BOARD_WHITE = "W"
-#added the following to be able to access easily
+
+# Masks for 64-bit bitboards and accessing specific rows.
 MASK64 = 0xFFFFFFFFFFFFFFFF
 NOT_A_FILE = 0xFEFEFEFEFEFEFEFE
 NOT_B_FILE = 0xFDFDFDFDFDFDFDFD
@@ -14,8 +15,6 @@ NOT_G_FILE = 0xBFBFBFBFBFBFBFBF
 NOT_H_FILE = 0x7F7F7F7F7F7F7F7F
 NOT_AB_FILE = NOT_A_FILE & NOT_B_FILE
 NOT_GH_FILE = NOT_G_FILE & NOT_H_FILE
-# getting rid of the board lookup -j
-
 
 
 def board_from_file(filename: str) -> tuple[int, int]:
@@ -31,8 +30,7 @@ def board_from_file(filename: str) -> tuple[int, int]:
         if len(row) != BOARD_SIZE:
             raise ValueError("Board file must contain exactly 8 rows of 8 characters.")
 
-    #changing this to starting as 0,0 -j
-    #initializing the boards
+    # Initializing the boards
     blackBoard, whiteBoard = 0,0
 
     for row in range(BOARD_SIZE):
@@ -61,8 +59,6 @@ def board_opponent(player: str) -> str:
 def board_in_bounds(row: int, col: int) -> bool:
     return 0 <= row < BOARD_SIZE and 0 <= col < BOARD_SIZE
 
-#getting rid of board_piece_at, only used once for a print function
-
 #both of the below work with the 64 bit board
 def board_count_pieces(board: int) -> int:
     """
@@ -73,13 +69,12 @@ def board_count_pieces(board: int) -> int:
 
 def board_is_initial_removal_phase(blackBoard: int, whiteBoard: int) -> bool:
     """
-    If the game still has 64 pieces, the game is in the initial removal phase.
-    Returns True if the board has 64 pieces.
+    If the game still has 63 or more pieces, the game is in the initial removal phase.
+    Returns True if the board has over 63 pieces.
     """
     total = board_count_pieces(blackBoard) + board_count_pieces(whiteBoard)
     
-    return (total >= 63)    # Change this to 63 if it turns out both players get to remove 1
-    #changed it to 63! -j
+    return (total >= 63)
 
 
 def board_legal_moves(blackBoard: int, whiteBoard: int, player: str) -> List[Move]:
@@ -92,11 +87,11 @@ def board_legal_moves(blackBoard: int, whiteBoard: int, player: str) -> List[Mov
     
     return board_legal_jumps(blackBoard, whiteBoard, player)
 
-#come back to this -j
+
 def board_legal_removals(player: str) -> List[Move]:
     """
-    In this version of Konane, only the agent gets to remove a piece, so we don't need to check the board.
-    The only valid pieces are in the middle 2x2 square. Returns a list containing these Moves.
+    Returns Moves representing the removals the given player (B or W) may take.
+    The first player gets to take one of the center 4 pieces, then the other player must take an adjacent piece.
     """
     if player == BOARD_BLACK:
         #remove d5 or e4
@@ -122,41 +117,35 @@ def board_legal_jumps(blackBoard: int, whiteBoard: int, player: str) -> List[Mov
         player_bits = whiteBoard
         opp_bits = blackBoard
 
-    #updated for 64 bit, 
+    # Get all empty spaces 
     empty = ~(player_bits | opp_bits) & MASK64
 
-    # Landing shift, victim shift, mask, row delta, and column delta (amount they moved)
+    # Opponent bit shift, mask, row delta, and column delta (amount they moved)
     # The masks prevent the edges from wrapping around when shifting left or right.
-    
-    #updating with 64 bit boards in mind
-    #doubled the masks for 64 bits
     directions = [
         (-8, 0xFFFFFFFFFFFFFFFF, -2, 0),    # Up
         (8, 0xFFFFFFFFFFFFFFFF, 2, 0),  # Down
         (-1, NOT_AB_FILE, 0, -2),    # Left
         (1, NOT_GH_FILE, 0, 2)   # Right    
-        ]
+    ]
     
     moves = []
 
-    
 
     # Move the entire board at once and find valid moves
     for l_shift, mask, d_row, d_col in directions:
         # player_bits & mask checks the player's starting positions and prevents wraparound.
-        # opp_bits << offset checks if there's an opponent in the path.
-        # empty << 1 ensures there's an empty space on the other side.
-        # Odd and even rows have to be checked separately because of the alternating nature of the board
-        # and the use of two separate, non-overlapping bitboards.
-        #l_shift will be the shift value check for opponent board
-        #d_row is for vertical checks
-        #d_col is for horizontal checks
+        # opp_bits << l_shift checks if there's an opponent in the path.
+        # empty << l_shift * 2 ensures there's an empty space on the other side.
+        # l_shift is the amount to shift the opponent board by.
+        # d_row is the number of rows moved across (vertical distance travelled)
+        # d_col is the number of columns moved across (horizontal distance travelled)
       
-        #this actually takes care of all 4 cases
-        #this takes care of down and right
+        # Check down and right
         if l_shift > 0:
             jumpable_bits = (player_bits & mask) & (opp_bits >> l_shift) & (empty >> l_shift * 2)
-        #this one takes care of left and up
+        
+        # Check left and up
         else:
             l_shift = abs(l_shift)
             jumpable_bits = (player_bits & mask) & (opp_bits << l_shift) & (empty << l_shift * 2)
@@ -242,13 +231,10 @@ def board_apply_move(blackBoard: int, whiteBoard: int, move: Move) -> tuple[int,
         raise ValueError("Jump leaves board bounds.")
     
     
-    # Determine the direction of the jump to check for chains.
-
-# Determine the step direction of the jump (-1, 0, or 1)
+    # Determine the step direction of the jump (-1, 0, or 1)
     d_row = 0 if s_row == e_row else (e_row - s_row) // abs(e_row - s_row)
     d_col = 0 if s_col == e_col else (e_col - s_col) // abs(e_col - s_col)
     curr_row, curr_col = s_row, s_col
-
 
 
     # Perform as many jumps in a straight line as possible.
